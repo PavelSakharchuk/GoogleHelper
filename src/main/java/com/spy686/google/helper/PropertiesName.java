3a0f1773fbbacd8ffdba1838fae5a12887c3fd0c
Pavel Sakharchuk <sakharchuk.pavel@gmail.com>
2021-06-20 16:20:50 +0300

    delete javax.xml.bind

--- src/main/java/com/spy686/google/helper/PropertiesName.java
+++ src/main/java/com/spy686/google/helper/PropertiesName.java
@@ -2,5 +2,5 @@ package com.spy686.google.helper;
 
 public class PropertiesName {
     //property names
-    public static final String APPLICATION_CONFIG = "application";
+    public static final String APPLICATION_CONFIG = "application_";
 }
